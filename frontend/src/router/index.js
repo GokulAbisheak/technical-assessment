b2978cbf4e4f17f12358f7150918ac8cb1ddf123
Gokul_Abisheak <gokul.abisheak@codeworxs.com>
2025-04-18 03:23:56 +0530

    updated routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,34 +1,68 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import AdminLogin from "@/views/AdminLogin.vue";
+import ListUsers from "@/views/ListUsers.vue";
 import AddUser from "@/views/AddUser.vue";
 import UpdateUser from "@/views/UpdateUser.vue";
-import ListUsers from "@/views/ListUsers.vue";
+import AdminLayout from "@/layout/AdminLayout.vue";
 
 const routes = [
   {
     path: "/",
-    redirect: "/users",
+    redirect: "/login"
   },
   {
-    path: "/users",
-    name: "ListUsers",
-    component: ListUsers,
+    path: "/login",
+    name: "AdminLogin",
+    component: AdminLogin,
+    meta: { requiresGuest: true }
   },
   {
-    path: "/users/add",
-    name: "AddUser",
-    component: AddUser,
-  },
-  {
-    path: "/users/update/:id",
-    name: "UpdateUser",
-    component: UpdateUser,
-  },
+    path: "/user",
+    component: AdminLayout,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: "",
+        redirect: "/users"
+      },
+      {
+        path: "/users",
+        name: "ListUsers",
+        component: ListUsers,
+        meta: { requiresAuth: true }
+      },
+      { 
+        path: "/users/add", 
+        name: "AddUser",
+        component: AddUser,
+        meta: { requiresAuth: true }
+      },
+      { 
+        path: "/users/update/:id",
+        name: "UpdateUser",
+        component: UpdateUser,
+        meta: { requiresAuth: true }
+      }
+    ]
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes
 });
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('admin_token')
+  
+  if (to.meta.requiresAuth && !token) {
+    next('/admin/login')
+  } else if (to.meta.requiresGuest && token) {
+    next('/users')
+  } else {
+    next()
+  }
+})
 
 export default router;
