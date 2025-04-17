f9a73f44047654f5590e9a307403f7a07a02795a
Gokul_Abisheak <gokul.abisheak@codeworxs.com>
2025-04-18 01:40:09 +0530

    minor change to full screen spinner

--- frontend/src/components/FullScreenSpinner.vue
+++ frontend/src/components/FullScreenSpinner.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex justify-center items-center min-h-screen w-full bg-[rgba(0,0,0,0.7)] absolute top-0 left-0"
+    class="flex justify-center items-center min-h-screen w-full bg-[rgba(0,0,0,0.7)] fixed top-0 left-0"
   >
     <div
       class="w-40 h-40 border-15 border-sky-600 border-t-gray-500 rounded-full animate-spin"
