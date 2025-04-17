0d70e13c199b75236c673fe20bae89e870a3a7b3
Gokul_Abisheak <gokul.abisheak@codeworxs.com>
2025-04-17 21:52:26 +0530

    created full spinner component

---
+++ frontend/src/components/FullScreenSpinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="flex justify-center items-center min-h-screen w-full bg-[rgba(0,0,0,0.7)] absolute top-0 left-0"
+  >
+    <div
+      class="w-40 h-40 border-15 border-sky-600 border-t-gray-500 rounded-full animate-spin"
+    ></div>
+  </div>
+</template>
